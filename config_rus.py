d9c19fd5fa951e77bc9c838b103fe4056179a17e
Shtefan <58820458+SHTFN@users.noreply.github.com>
2023-06-20 20:29:58 +0300

    20.06

--- config_rus.py
+++ config_rus.py
@@ -9,5 +9,10 @@ messages = {
     'phone_num_access': 'Номер телефона {num} успешно добавлен ✅',
     'info': 'Ваши данные:\n'
             'Telegram id: {user_id}\n'
-            'Номер телефона: {user_phone_num}'
+            'Номер телефона: {user_phone_num}',
+    'points': 'Ваше количество баллов: {count_points}',
+    'admin_panel': 'Вы вошли в админ-панель',
+    'create_sendall': 'Для создания рассылки введите команду /sendall <code>Ваш текст</code>',
+    'error_sendall': 'Ошибка: вы не ввели рассылаемый текст',
+
 }
